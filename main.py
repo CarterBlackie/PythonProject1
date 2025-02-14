2906cf2490b6afbf2e26d73f902de90eb164b039
Carter Blackie <carterblackie55@gmail.com>
2025-02-14 16:24:17 -0500

    Added Task 1 and Task 2
    Made task 1 which handles only printing to the user
    
    Made task 2 which takes user input and prints out the input to the user

---
+++ main.py
@@ -0,0 +1,22 @@
+'''
+Purpose: Learning basic features of Python
+Author: Carter Blackie
+'''
+
+#Task 1: Printing
+
+print(f'The book title is "Learn Python in 21 days".')
+print(f"What's mine is mine, and whats yours is mine.")
+print(f'''You have enemies? Good. That means you've stood up for something, sometime in your life" Winston Churchill''')
+print("""Three things cannot be long hidden:
+      the sun, 
+      the moon,
+      and the truth""")
+
+
+#Task 2: User Input
+
+food = input("Enter your favorite food: ")
+movie = input("Enter your favorite TV show: ")
+
+print(f'I like to eat {food} while watching {movie}')
